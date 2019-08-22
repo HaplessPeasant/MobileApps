8bcd28ab6298ca5dd2221803d13b4e61330f49a2
HaplessPeasant <49355057+HaplessPeasant@users.noreply.github.com>
2019-08-22 11:44:49 -0400

    Add files via upload

---
+++ pizzaFunctions.js
@@ -0,0 +1,9 @@
+//changes page color to tomato
+function setTomato() {
+	heading.style.background = "tomato";
+}
+
+//changes page color to wheat
+function setWheat()	{
+	heading.style.background = "wheat";
+}
